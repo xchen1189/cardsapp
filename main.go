37e4af5796edca89783fc733271655fc6b33a7c8
bcat <bcat1189@gamil.com>
2024-12-03 00:38:45 -0600

    Dummy Commit

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 func main() {
-	//cards := deck{"Ace of Diamonds", newCard()}
-	//cards = append(cards, "Six of Spades")
+	// cards := deck{"Ace of Diamonds", newCard()}
+	// cards = append(cards, "Six of Spades")
 
-	//cards := NewDeck()
-	//cards.print()
-	//hand, remainning := deal(cards, 5)
-	//hand.print()
-	//remainning.print()
-	//cards.saveToFile("my_cards")
+	// cards := NewDeck()
+	// cards.print()
+	// hand, remainning := deal(cards, 5)
+	// hand.print()
+	// remainning.print()
+	cards.saveToFile("my_cards")
 	cards := newDeckFromFile("my_cards")
 	cards.print()
 	cards.shuffle()
